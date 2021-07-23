95b353666eeb8373f299f84c6a47add775fd5f1f
evandikt <evandikt@gmail.com>
2021-07-23 13:25:49 +0200

    Update evntboard-communicate version

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,7 @@
-import process from 'process';
-import { EvntComClient, EvntComServer } from "evntboard-communicate";
+import { getEvntComServerFromChildProcess } from "evntboard-communicate";
 import open from "open";
 
-// parse params
-const { name: NAME, customName: CUSTOM_NAME, config: {} } = JSON.parse(process.argv[2]);
-const EMITTER = CUSTOM_NAME || NAME;
-
-// create Client and Server COM
-const evntComClient = new EvntComClient(
-    (cb: any) => process.on('message', cb),
-    (data: any) => process.send(data),
-);
-
-const evntComServer = new EvntComServer();
-
-evntComServer.registerOnData((cb: any) => process.on('message', async (data) => {
-    const toSend = await cb(data);
-    if (toSend) process.send(toSend);
-}));
-
-evntComServer.expose("newEvent", () => {});
-evntComServer.expose("load", () => {});
-evntComServer.expose("unload", () => {});
-evntComServer.expose("reload", () => {});
+const evntComServer = getEvntComServerFromChildProcess();
 
 evntComServer.expose("open", async (target:string, options?: {}) => {
     return open(target, options)
